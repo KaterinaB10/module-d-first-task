c90405e440afca7d419313f62b0759fd3629ed14
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-03-08 10:22:38 +0100

    updated

--- first-task/src/App.jsx
+++ first-task/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import { NavBar } from "./components/nav-bar/NavBar";
+import { UserAccount } from "./components/user-account/UserAccount";
 
 function App() {
   return (
     <>
       <header className="header">
         <NavBar />
+        <UserAccount />
       </header>
       <main className="main"></main>
       <footer className="footer"></footer>
