7a63f7e2dd9c739fdb295830530e7b0a727fb0ca
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-03-14 11:31:25 +0100

    updated

--- src/components/menu-card/CardMenu.jsx
+++ src/components/menu-card/CardMenu.jsx
@@ -4,34 +4,39 @@ import coffeeTypes from "./coffeAPI.json";
 import { PopUpDescription } from "./PopUpDescription";
 
 export function CardMenu() {
-  const [readMoreButton, setReadMoreButton] = useState(false);
-
   return (
     <div className="menu-cards">
       {coffeeTypes.coffeeTypes.map((coffeeType) => {
-        return (
-          <div className="card-container" key={coffeeType.id}>
-            <div className="card-price-container">
-              <p className="card-price">{coffeeType.price}</p>
-            </div>
-            <div className="card-description-container">
-              <h2 className="card-description-title">{coffeeType.title}</h2>
-              <button
-                className="card-description-read-more-btn"
-                onMouseEnter={() => setReadMoreButton(true)}
-                onMouseLeave={() => setReadMoreButton(false)}
-              >
-                Read more
-              </button>
-            </div>
-            <PopUpDescription trigger={readMoreButton}>
-              <div className="pop-up-text">
-                <p>{coffeeType.description}</p>
-              </div>
-            </PopUpDescription>
-          </div>
-        );
+        return <MenuItem key={coffeeType.id} coffeeType={coffeeType} />;
       })}
+    </div>
+  );
+}
+
+function MenuItem(props) {
+  const { coffeeType } = props;
+  const [readMoreButton, setReadMoreButton] = useState(false);
+
+  return (
+    <div className="card-container">
+      <div className="card-price-container">
+        <p className="card-price">{coffeeType.price}</p>
+      </div>
+      <div className="card-description-container">
+        <h2 className="card-description-title">{coffeeType.title}</h2>
+        <button
+          className="card-description-read-more-btn"
+          onMouseEnter={() => setReadMoreButton(true)}
+          onMouseLeave={() => setReadMoreButton(false)}
+        >
+          Read more
+        </button>
+      </div>
+      <PopUpDescription trigger={readMoreButton}>
+        <div className="pop-up-text">
+          <p>{coffeeType.description}</p>
+        </div>
+      </PopUpDescription>
     </div>
   );
 }
